302aa33b8667850ce1818dfa893890b11d742246
mattew8 <mattew4483@likelion.org>
2021-01-01 15:59:03 +0900

    [mdf] base.html추가 및 상속

--- blogsproject/blogsproject/urls.py
+++ blogsproject/blogsproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('blogs/', include("blogs.urls")),
 ]
---
+++ blogsproject/blogsproject/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+
+# TemplateView
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['app_list'] = ['blogs']
+
+        return context
