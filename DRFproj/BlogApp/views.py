8be9918142e5e7c91ce592424329300e080b8bcd
mattew8 <mattew4483@likelion.org>
2021-01-23 15:54:27 +0900

    [mdf] DRF Mixins 사용

--- DRFproj/BlogApp/views.py
+++ DRFproj/BlogApp/views.py
@@ -1,56 +1,50 @@
 from django.shortcuts import render
-from .api.serializers import PostSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-# APIView : 공통적인 함수 재사용 o, DRY 정책 따르도록 하는 강력한 패턴 제공 
-from .models import Post
 from django.http import Http404
+
+from rest_framework.response import Response
 from rest_framework import status
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
-# DRF API view -> 두 종류의 wrapper 선택 가능
-# 1. 함수형 : @api_view 데코레이터 사용
-# 2. 클래스형 : APIView 클래스 상속
+from .models import Post
+from .api.serializers import PostSerializer
 
+# DRF mixins!
+# APIView에서는 각 요청 method에 맞게 serializer에서 직접 처리
+# but 자주 사용되는 기능이라 DRF에서 미리 구현 -> mixins !
+# queryset과 serializer_class를 지정해주기만하면 나머지는 상속받은 Mixin과 연결해주기만 하면 됨!
 
-
-class PostList(APIView):
-    def get(self, request, format=None):
-        # GET 요청일 때...
-        post = Post.objects.all()
-        serializer = PostSerializer(post, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        # POST 요청일 때...
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_201_CREATED)
-
-class PostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Http404
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
     
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+    # 요청 method에 맞게 함수를 정의!
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+ 
+class PostDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
+                mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
     
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+    
+ 
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer      
