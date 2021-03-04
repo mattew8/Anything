7c6c703a559e19753f942486e0f8c1dd491067bf
mattew8 <mattew4483@likelion.org>
2021-03-04 13:37:02 +0900

    [mdf] 농구 게임 기능 구현 완료

--- js/js/자바스크립트 프로젝트 가이드/농구 게임 만들기/basketball.js
+++ js/js/자바스크립트 프로젝트 가이드/농구 게임 만들기/basketball.js
@@ -3,11 +3,23 @@ var textElem = document.getElementById('text');
 
 // 컴퓨터 점수
 var comScore = 0;
+// 컴퓨터 차례
+var isComTurn = true;
+var comBtn = document.getElementsByClassName('btn-com');
 
 // 유저 점수
 var userScore = 0;
+var userBtn = document.getElementsByClassName('btn-user');
+
+// 남은 슛 횟수
+var shotsLeft = 15;
 
 function onCumputerShoot(){
+    // 컴퓨터 차례가 아니면 -> 함수작동x 바로 return
+    if (isComTurn === false){
+        return;
+    }
+
     // 컴퓨터는 2점슛, 3점슛 중 랜덤하게 선택
     var shootType = Math.random() < 0.5 ? 2 : 3;
 
@@ -30,9 +42,25 @@ function onCumputerShoot(){
             textElem.innerHTML = '컴퓨터 3점슛 실패!';
         }
     }
+    isComTurn = false;
+    
+    // 컴퓨터가 슛한 후 버튼 비활성화 - getElementsByClassName은 선택된 elem가 하나라도 리턴값은 복수! -> for문으로 받아줘야 함
+    for (var i=0; i < comBtn.length; i++){
+        comBtn[i].disabled = true;
+    }
+
+    // 유저가 슛한 후 버튼 활성화
+    for (var i=0; i < userBtn.length; i++){
+        userBtn[i].disabled = false
+    }
 }
 
 function onUserShoot(shootType){
+    // 컴퓨터 차례면 -> 함수 작동x 바로 return
+    if (isComTurn === true){
+        return;
+    }
+
     var userScoreElem = document.getElementById('user-score');
 
     if (shootType === 2){
@@ -50,6 +78,42 @@ function onUserShoot(shootType){
             userScoreElem.innerHTML = userScore;
         } else {
             textElem.innerHTML = '3점슛 실패!';
+        }
+    }
+
+    isComTurn = true;
+    
+    // 컴퓨터가 슛한 후 버튼 활성화
+    for (var i=0; i < comBtn.length; i++){
+        comBtn[i].disabled = false;
+    }
+
+    // 유저가 슛한 후 버튼 비활성화
+    for (var i=0; i < userBtn.length; i++){
+        userBtn[i].disabled = true;
+    }
+
+    // 남은 슛 횟수 차감
+    shotsLeft --;
+
+    var shotsLeftElem = document.getElementById('left');
+    shotsLeftElem.innerHTML = shotsLeft;
+
+    // 남은 shots가 없을 경우
+    if (shotsLeft === 0){
+        for (var i=0; i < userBtn.length; i++){
+            userBtn[i].disabled = true;
+        }
+        for (var i=0; i < comBtn.length; i++){
+            comBtn[i].disabled = true;
+        }
+
+        if (comScore < userScore){
+            textElem.innerHTML = "승리!";
+        } else if (comScore > userScore){
+            textElem.innerHTML = "패배...";
+        } else {
+            textElem.innerHTML = "비김";
         }
     }
 }
